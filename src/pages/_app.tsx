6c045c8fe1c99479203f8cd083b7b610f903e78c
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-06-10 20:38:13 -0300

    chore(next/font): Changed global font from Inter to Roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Roboto({
+  weight: ['500', '700'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
