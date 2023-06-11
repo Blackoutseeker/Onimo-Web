7fc3d2acba1ebc97e140aaab2aa5b6bffa7162f4
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-06-11 02:10:26 -0300

    feat(pages): Changed body background color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head />
-      <body>
+      <body className="bg-dark">
         <Main />
         <NextScript />
       </body>
