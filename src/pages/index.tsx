cb3a607c9ef7b9b998844cab3cd8d7d40ae8d124
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-06-11 02:11:16 -0300

    feat(pages): Added a title to the Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <Head>
+        <title>Onimo</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </>
   )
 }
 
