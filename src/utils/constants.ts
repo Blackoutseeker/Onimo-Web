2ebbdb2d62cf280a21a0dbdc0e5ecee324e334d7
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-10 05:51:20 -0300

    feat(utils): Add rules constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const rules: string[] = [
+  'Preste respeito a todos os usuários.',
+  'Não revele sua verdadeira identidade.',
+  'Não divulgue suas informações pessoais, como endereço, número de telefone, documentos e etc.',
+  'Não divulgue conteúdos ilícitos, ofensivos, violentos, sexuais, e dentre outros desconfortáveis ou repudiantes.',
+  'Não utilize o chat para fins comerciais ou publicitários.',
+  'Não use linguagem abusiva ou palavrões.',
+  'Embora o chat seja anônimo, suas ações podem ser rastreadas e, se necessário, medidas legais podem ser tomadas.'
+]
