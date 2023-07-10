3fc24c414b0da4162c07269d4b76c009ae8f4a8b
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-10 05:51:46 -0300

    feat(utils): Add information constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,14 @@ export const rules: string[] = [
   'Não use linguagem abusiva ou palavrões.',
   'Embora o chat seja anônimo, suas ações podem ser rastreadas e, se necessário, medidas legais podem ser tomadas.'
 ]
+
+export const information: string[] = [
+  'Todos os dias você recebe um novo "apelido de usuário" pré-definido. Ele estará situado no canto superior esquerdo da tela principal.',
+  'Não é possível alterar seu apelido de usuário manualmente.',
+  'Não é possível alterar mensagens.',
+  'Não é possível excluir mensagens.',
+  'É possível criar no máximo 10 salas públicas, cada uma comportando 5 usuários simultaneamente.',
+  'Salas privadas comportam no máximo 10 usuários simultaneamente.',
+  'Quando todos os usuários de uma sala privada se desconectam, ela será excluída automaticamente.',
+  'Todas as salas e mensagens são deletadas automaticamente às 00:00h diariamente.'
+]
