1fe173c0a96f405a26664054f65f2ae93d0112da
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-10 09:32:50 -0300

    test(unit): Add a test to check for duplications in constants

--- src/__tests__/unit/constants.test.ts
+++ src/__tests__/unit/constants.test.ts
@@ -1,9 +1,29 @@
 import { rules, information, nicknames } from '@/utils/constants'
 
+const checkDuplication = (list: string[]): boolean => {
+  for (const item of list) {
+    const duplicationOccurrences: number = list.filter(
+      itemFromFilter => itemFromFilter.toLowerCase() === item.toLowerCase()
+    ).length
+
+    if (duplicationOccurrences > 1) {
+      return true
+    }
+  }
+
+  return false
+}
+
 describe('Constants', () => {
   test('Constants must not be empty', () => {
     expect(rules.length).toBeGreaterThan(0)
     expect(information.length).toBeGreaterThan(0)
     expect(nicknames.length).toBeGreaterThan(0)
+  })
+
+  test('Constants must not be duplicated', () => {
+    expect(checkDuplication(rules)).toBeFalsy()
+    expect(checkDuplication(information)).toBeFalsy()
+    expect(checkDuplication(nicknames)).toBeFalsy()
   })
 })
