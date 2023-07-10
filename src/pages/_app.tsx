f51f138de889f468bbff8dd8ce6c8a12f9c78df5
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-10 05:50:23 -0300

    refactor(pages): Change constant name from inter to roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
 
-const inter = Roboto({
+const roboto = Roboto({
   weight: ['300', '500', '700'],
   subsets: ['latin']
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main className={roboto.className}>
       <Component {...pageProps} />
     </main>
   )
