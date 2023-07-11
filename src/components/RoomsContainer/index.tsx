25fa8a0099b9755b12f9e6943c22220f62c4d4bf
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-11 09:20:36 -0300

    feat(components): Add InfoModal to RoomsContainer component

--- src/components/RoomsContainer/index.tsx
+++ src/components/RoomsContainer/index.tsx
@@ -1,38 +1,52 @@
-import type { FC } from 'react'
+import { FC, useRef } from 'react'
 import { AiOutlineInfoCircle, AiOutlinePlus } from 'react-icons/ai'
 import RoomsList from '../RoomsList'
+import InfoModal from '../InfoModal'
 
 const RoomsContainer: FC = () => {
+  const modalReference = useRef<HTMLDialogElement>(null)
+
+  const showModal = () => {
+    modalReference.current?.showModal()
+  }
+
   return (
-    <div
-      className="flex flex-col h-screen md:h-[calc(100vh-128px)] w-full md:max-w-[300px]
-      border-r-[1px] border-soft-dark"
-    >
-      <header
-        className="flex items-center justify-between border-b-[1px] 
-      border-soft-dark h-16 px-10"
+    <>
+      <div
+        className="flex h-screen w-full flex-col border-r-[1px] border-soft-dark
+        md:h-[calc(100vh-128px)] md:max-w-[300px]"
       >
-        <h3 className="text-xl font-bold text-white">john_doe7</h3>
-        <button title="Informações" aria-label="Informações">
-          <AiOutlineInfoCircle className="text-soft-gray" size={24} />
+        <header
+          className="flex h-16 items-center justify-between 
+          border-b-[1px] border-soft-dark px-10"
+        >
+          <h3 className="text-xl font-bold text-white">john_doe7</h3>
+          <button
+            title="Informações"
+            aria-label="Informações"
+            onClick={showModal}
+          >
+            <AiOutlineInfoCircle className="text-soft-gray" size={24} />
+          </button>
+        </header>
+
+        <button className="flex items-center space-x-5 px-10 py-5 duration-150 hover:bg-soft-dark">
+          <div>
+            <AiOutlinePlus className="text-white" size={20} />
+          </div>
+          <h4 className="text-left font-bold text-white">Criar sala pública</h4>
         </button>
-      </header>
+        <button className="flex items-center space-x-5 px-10 py-5 duration-150 hover:bg-soft-dark">
+          <div>
+            <AiOutlinePlus className="text-white" size={20} />
+          </div>
+          <h4 className="text-left font-bold text-white">Criar sala privada</h4>
+        </button>
 
-      <button className="flex items-center px-10 py-5 space-x-5 duration-150 hover:bg-soft-dark">
-        <div>
-          <AiOutlinePlus className="text-white" size={20} />
-        </div>
-        <h4 className="font-bold text-left text-white">Criar sala pública</h4>
-      </button>
-      <button className="flex items-center px-10 py-5 space-x-5 duration-150 hover:bg-soft-dark">
-        <div>
-          <AiOutlinePlus className="text-white" size={20} />
-        </div>
-        <h4 className="font-bold text-left text-white">Criar sala privada</h4>
-      </button>
-
-      <RoomsList />
-    </div>
+        <RoomsList />
+      </div>
+      <InfoModal modalReference={modalReference} />
+    </>
   )
 }
 
