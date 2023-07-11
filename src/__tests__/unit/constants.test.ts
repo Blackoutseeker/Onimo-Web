2797830e66b1dec9dc02399fedb8d04f4b7d3947
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-11 05:01:12 -0300

    test(unit): Add a test to check if nicknames respect a regex

--- src/__tests__/unit/constants.test.ts
+++ src/__tests__/unit/constants.test.ts
@@ -14,6 +14,17 @@ const checkDuplication = (list: string[]): boolean => {
   return false
 }
 
+const checkIfRespectRegex = (list: string[], regex: RegExp): boolean => {
+  for (const item of list) {
+    const notMatchRegex: boolean = !RegExp(regex.source).test(item)
+    if (notMatchRegex) return false
+  }
+
+  return true
+}
+
+const nicknamesRegex: RegExp = /^[a-z]+_[a-z]+$/g
+
 describe('Constants', () => {
   test('Constants must not be empty', () => {
     expect(rules.length).toBeGreaterThan(0)
@@ -25,5 +36,9 @@ describe('Constants', () => {
     expect(checkDuplication(rules)).toBeFalsy()
     expect(checkDuplication(information)).toBeFalsy()
     expect(checkDuplication(nicknames)).toBeFalsy()
+  })
+
+  test(`Nicknames must respect the ${nicknamesRegex.source} regex`, () => {
+    expect(checkIfRespectRegex(nicknames, nicknamesRegex)).toBeTruthy()
   })
 })
