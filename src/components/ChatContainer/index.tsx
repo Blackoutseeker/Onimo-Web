451b1d550b331cdb64921e6748ca11d619fda3ca
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-13 09:11:18 -0300

    feat(components): Add Header to ChatContainer component

--- src/components/ChatContainer/index.tsx
+++ src/components/ChatContainer/index.tsx
@@ -1,7 +1,12 @@
 import type { FC } from 'react'
+import Header from './Header'
 
 const ChatContainer: FC = () => {
-  return <div className="flex flex-1 flex-col"></div>
+  return (
+    <div className="flex flex-1 flex-col">
+      <Header />
+    </div>
+  )
 }
 
 export default ChatContainer
