789dd2fab028a264d4223ad5510eb37c61d00aaa
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-14 12:50:59 -0300

    feat(components): Create VoidChat component

---
+++ src/components/ChatContainer/VoidChat/index.tsx
@@ -0,0 +1,14 @@
+import type { FC } from 'react'
+
+const VoidChat: FC = () => {
+  return (
+    <div className="flex flex-1 items-center justify-center p-10">
+      <p className="max-w-xs text-center text-soft-gray">
+        Sempre mantenha o respeito aos demais usuários. Antes de enviar uma
+        mensagem, pense em suas consequências.
+      </p>
+    </div>
+  )
+}
+
+export default VoidChat
