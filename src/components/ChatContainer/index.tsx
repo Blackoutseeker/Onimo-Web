a06ed01e6c855aa579f789fc9881faf1f0ee1fa3
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-14 12:52:06 -0300

    feat(components): Add VoidChat to ChatContainer component

--- src/components/ChatContainer/index.tsx
+++ src/components/ChatContainer/index.tsx
@@ -1,10 +1,12 @@
 import type { FC } from 'react'
 import Header from './Header'
+import VoidChat from './VoidChat'
 
 const ChatContainer: FC = () => {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col space-y-5">
       <Header />
+      <VoidChat />
     </div>
   )
 }
