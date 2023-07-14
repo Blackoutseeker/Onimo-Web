5a98eded3cb17851f96051cfc7ec0fbdcbabccfc
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-14 13:52:15 -0300

    feat(components): Add Footer to ChatContainer component

--- src/components/ChatContainer/index.tsx
+++ src/components/ChatContainer/index.tsx
@@ -1,12 +1,14 @@
 import type { FC } from 'react'
 import Header from './Header'
 import VoidChat from './VoidChat'
+import Footer from './Footer'
 
 const ChatContainer: FC = () => {
   return (
     <div className="flex flex-1 flex-col space-y-5">
       <Header />
       <VoidChat />
+      <Footer />
     </div>
   )
 }
