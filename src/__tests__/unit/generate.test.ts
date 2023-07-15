114c6404a748fdd2e5e5f39b07b82e54b52bac37
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-14 22:12:06 -0300

    test(unit): Add a test to check if the ID is empty

--- src/__tests__/unit/generate.test.ts
+++ src/__tests__/unit/generate.test.ts
@@ -5,4 +5,10 @@ describe('Testing methods of the "generate" module', () => {
     const id = generateId()
     expect(id.length).toBeGreaterThan(0)
   })
+
+  test('"generateId" must use length provided via argument', () => {
+    const idLength = Math.floor(Math.random() * 30) + 1
+    const id = generateId(idLength)
+    expect(id.length).toBe(idLength)
+  })
 })
