4e00139055c4cde20b6a29078ebc3b45e7944d3d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-14 22:14:18 -0300

    test(unit): Add a test to check if the ID respect a regex

--- src/__tests__/unit/generate.test.ts
+++ src/__tests__/unit/generate.test.ts
@@ -1,5 +1,12 @@
 import { generateId } from '@/utils/generate'
 
+const checkIfRespectRegex = (text: string, regex: RegExp): boolean => {
+  const matchRegex: boolean = RegExp(regex.source).test(text)
+  return matchRegex
+}
+
+const idRegex: RegExp = /^[a-zA-Z0-9]+$/g
+
 describe('Testing methods of the "generate" module', () => {
   test('"generateId" must not return an empty string', () => {
     const id = generateId()
@@ -10,5 +17,9 @@ describe('Testing methods of the "generate" module', () => {
     const idLength = Math.floor(Math.random() * 30) + 1
     const id = generateId(idLength)
     expect(id.length).toBe(idLength)
+  })
+
+  test(`ID must respect the ${idRegex.source} regex`, () => {
+    expect(checkIfRespectRegex(generateId(), idRegex)).toBeTruthy()
   })
 })
