29ff1e444739f563f994e0acb421d5131c22924a
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-14 22:44:34 -0300

    feat(utils): Create generateNickname method

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -1,3 +1,5 @@
+import { nicknames } from './constants'
+
 const chars = Array.from(Array(26), (_, i) => String.fromCharCode(65 + i))
   .concat(Array.from(Array(26), (_, i) => String.fromCharCode(97 + i)))
   .concat(Array.from(Array(10), (_, i) => String.fromCharCode(48 + i)))
@@ -12,4 +14,12 @@ export const generateId = (length: number = 12): string => {
   }
 
   return id
+}
+
+export const generateNickname = (): string => {
+  const randomIndex = Math.floor(Math.random() * nicknames.length)
+  let randomNickname: string = nicknames[randomIndex]
+  const randomDigit = Math.floor(Math.random() * 9) + 1
+  randomNickname += randomDigit
+  return randomNickname
 }
