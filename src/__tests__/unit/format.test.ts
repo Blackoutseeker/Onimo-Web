6cb0144813e3163296c2c3e5a446e2c294ac8c42
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-16 11:59:28 -0300

    refactor(test): Change the constant name from result to time

--- src/__tests__/unit/format.test.ts
+++ src/__tests__/unit/format.test.ts
@@ -3,13 +3,13 @@ import { formatTime } from '@/utils/format'
 describe('Testing methods of the "format" module', () => {
   test('"formatTime" must pad with leading zeros when necessary', () => {
     const date = new Date('2023-07-15T15:05:00')
-    const result = formatTime(date)
-    expect(result).toBe('15:05')
+    const time = formatTime(date)
+    expect(time).toBe('15:05')
   })
 
   test('"formatTime" must return the formatted time correctly', () => {
     const dateString = '2023-07-15T08:30:00'
-    const result = formatTime(dateString)
-    expect(result).toBe('08:30')
+    const time = formatTime(dateString)
+    expect(time).toBe('08:30')
   })
 })
