6e5c7de5315122679938b5f7eae54c43b5daeee3
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-18 00:54:06 -0300

    chore(@types): Create global environment variables file

---
+++ src/@types/env-vars.d.ts
@@ -0,0 +1,19 @@
+export declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      FIREBASE_API_KEY: string
+      FIREBASE_AUTH_DOMAIN: string
+      FIREBASE_DATABASE_URL: string
+      FIREBASE_PROJECT_ID: string
+      FIREBASE_STORAGE_BUCKET: string
+      FIREBASE_MESSAGING_SENDER_ID: string
+      FIREBASE_APP_ID: string
+      FIREBASE_MEASUREMENT_ID: string
+      FIREBASE_APP_CHECK_DEBUG_TOKEN: string
+
+      RECAPTCHA_SITE_KEY: string
+
+      IS_TESTING_FROM_CI: boolean
+    }
+  }
+}
