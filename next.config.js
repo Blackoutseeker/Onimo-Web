947ee00ad829cf3cb6c17c454e425ad188e8cdf6
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-18 01:02:58 -0300

    chore(next): Add environment variables in the Next configuration file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+    FIREBASE_APP_CHECK_DEBUG_TOKEN: process.env.FIREBASE_APP_CHECK_DEBUG_TOKEN,
+
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+
+    IS_TESTING_FROM_CI: process.env.IS_TESTING_FROM_CI
+  }
 }
 
 module.exports = nextConfig
