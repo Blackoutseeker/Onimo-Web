77365a101e1c59a798d0e320d726b1122d89f52a
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-18 20:56:05 -0300

    feat(components): Add listenRoom method to RoomsList component

--- src/components/RoomsList/index.tsx
+++ src/components/RoomsList/index.tsx
@@ -1,5 +1,6 @@
-import { FC, memo, useState } from 'react'
+import { FC, useState, useEffect, memo } from 'react'
 import type { Room } from '@/entities/room'
+import { listenRooms } from '@/services/database/room'
 import { BsFillPersonFill } from 'react-icons/bs'
 
 interface RoomItemProps {
@@ -12,7 +13,7 @@ const RoomItem: FC<RoomItemProps> = ({ room }) => {
       <button className="flex w-full items-center space-x-5 px-10 py-5 duration-150 hover:bg-soft-dark">
         <h4 className="w-full text-left font-bold text-white">{room.name}</h4>
         <h5 className="text-left text-sm text-white">
-          {room.activeUsers.length}/5
+          {room.active_users?.length ?? 0}/5
         </h5>
         <div>
           <BsFillPersonFill className="text-white" size={20} />
@@ -26,6 +27,15 @@ const RoomItemMemoized = memo(RoomItem)
 
 const RoomsList: FC = () => {
   const [rooms, setRooms] = useState<Room[]>([])
+
+  useEffect(() => {
+    const handleRoomsListener = listenRooms(setRooms)
+    handleRoomsListener.on()
+    return () => {
+      handleRoomsListener.off()
+    }
+  }, [])
+
   const renderRoomItems = () =>
     rooms.map(room => <RoomItemMemoized key={room.id} room={room} />)
 
