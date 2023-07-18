bfac2e7211cb4ce730b22770bbc4d7f1bfbebf1d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-18 01:41:42 -0300

    feat(pages): Add initializeFirebaseAppCheck method to App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import { useEffect } from 'react'
+import { initializeFirebaseAppCheck } from '@/utils/firebase'
 
 const roboto = Roboto({
   weight: ['300', '500', '700'],
@@ -8,6 +10,10 @@ const roboto = Roboto({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initializeFirebaseAppCheck()
+  }, [])
+
   return (
     <main className={roboto.className}>
       <Component {...pageProps} />
