96f790a314aa89bdfc83a0f1cbf6eadee3f2543a
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-18 22:28:27 -0300

    feat(pages): Add Redux Provider to App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
 import { useEffect } from 'react'
 import { initializeFirebaseAppCheck } from '@/utils/firebase'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@/services/store'
 
 const roboto = Roboto({
   weight: ['300', '500', '700'],
@@ -15,8 +17,10 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [])
 
   return (
-    <main className={roboto.className}>
-      <Component {...pageProps} />
-    </main>
+    <ReduxProvider store={store}>
+      <main className={roboto.className}>
+        <Component {...pageProps} />
+      </main>
+    </ReduxProvider>
   )
 }
