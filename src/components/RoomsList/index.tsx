6eeb4f88e84ed8465cd72c7a094564313e67e171
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-18 22:45:38 -0300

    feat(components): Create changeRoom method

--- src/components/RoomsList/index.tsx
+++ src/components/RoomsList/index.tsx
@@ -1,16 +1,24 @@
 import { FC, useState, useEffect, memo } from 'react'
 import type { Room } from '@/entities/room'
-import { listenRooms } from '@/services/database/room'
 import { BsFillPersonFill } from 'react-icons/bs'
+import { useAppDispatch } from '@/hooks/redux'
+import { listenRooms } from '@/services/database/room'
+import { setCurrentRoom } from '@/services/store/ducks/room'
 
 interface RoomItemProps {
   room: Room
+  changeRoom: (room: Room) => void
 }
 
-const RoomItem: FC<RoomItemProps> = ({ room }) => {
+const RoomItem: FC<RoomItemProps> = ({ room, changeRoom }) => {
+  const handleChangeRoom = () => changeRoom(room)
+
   return (
     <li>
-      <button className="flex w-full items-center space-x-5 px-10 py-5 duration-150 hover:bg-soft-dark">
+      <button
+        className="flex w-full items-center space-x-5 px-10 py-5 duration-150 hover:bg-soft-dark"
+        onClick={handleChangeRoom}
+      >
         <h4 className="w-full text-left font-bold text-white">{room.name}</h4>
         <h5 className="text-left text-sm text-white">
           {room.active_users?.length ?? 0}/5
@@ -26,6 +34,7 @@ const RoomItem: FC<RoomItemProps> = ({ room }) => {
 const RoomItemMemoized = memo(RoomItem)
 
 const RoomsList: FC = () => {
+  const dispatch = useAppDispatch()
   const [rooms, setRooms] = useState<Room[]>([])
 
   useEffect(() => {
@@ -36,8 +45,14 @@ const RoomsList: FC = () => {
     }
   }, [])
 
+  const changeRoom = (room: Room) => {
+    dispatch(setCurrentRoom(room))
+  }
+
   const renderRoomItems = () =>
-    rooms.map(room => <RoomItemMemoized key={room.id} room={room} />)
+    rooms.map(room => (
+      <RoomItemMemoized key={room.id} room={room} changeRoom={changeRoom} />
+    ))
 
   if (rooms.length === 0)
     return (
