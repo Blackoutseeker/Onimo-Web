317a2324a73aadaed1c26a3584a5a27f41dc5e41
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-22 23:25:35 -0300

    test(integration): Add a test to verify that the user ID and nickname respect their regexes

--- src/__tests__/integration/generate.test.ts
+++ src/__tests__/integration/generate.test.ts
@@ -1,9 +1,16 @@
 import { generateUser } from '@/utils/generate'
+import { idRegex, generatedNicknameRegex } from '@/utils/constants'
 
 describe('Testing integrated methods of the "generate" module', () => {
   test('User ID and nickname must not be empty', () => {
     const user = generateUser()
     expect(user.id.length).toBeGreaterThan(0)
     expect(user.nickname.length).toBeGreaterThan(0)
+  })
+
+  test(`User ID must respect ${idRegex.source} regex and nickname must respect ${generatedNicknameRegex.source} regex`, () => {
+    const user = generateUser()
+    expect(user.id).toMatch(idRegex)
+    expect(user.nickname).toMatch(generatedNicknameRegex)
   })
 })
