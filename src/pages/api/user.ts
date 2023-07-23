00d57028221075f8c934081d1c73702fb81339e0
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-23 01:01:36 -0300

    feat(api): Create user API endpoint

---
+++ src/pages/api/user.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getUserFromToken } from '@/services/auth'
+
+const handler = async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method !== 'GET') {
+    return response.status(405).end()
+  }
+
+  const token = request.cookies.token
+  if (!token) {
+    return response.status(401).end('Token not found.')
+  }
+
+  const user = await getUserFromToken(token)
+  return response.status(200).json({ user })
+}
+
+export default handler
