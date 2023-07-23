91be499491d6c78d7cba8082d8c83e9ee6e44467
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-23 00:57:19 -0300

    feat(middleware): Create middleware.ts file

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { generateToken, getUserFromToken } from './services/auth'
+import { generateExpirationDate } from './utils/generate'
+
+export const middleware = async (request: NextRequest) => {
+  const token = request.cookies.get('token')?.value
+  if (!token) {
+    const newToken = await generateToken()
+    const expirationDate = generateExpirationDate()
+    const response = NextResponse.redirect(request.url)
+
+    response.cookies.set({
+      name: 'token',
+      value: newToken,
+      path: '/',
+      httpOnly: true,
+      expires: expirationDate,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production'
+    })
+
+    return response
+  }
+
+  const isTokenNotVerified = !getUserFromToken(token)
+  if (isTokenNotVerified) {
+    return NextResponse.json({ message: 'Invalid token.' }, { status: 401 })
+  }
+}
+
+export const config = {
+  matcher: ['/api/:function*']
+}
