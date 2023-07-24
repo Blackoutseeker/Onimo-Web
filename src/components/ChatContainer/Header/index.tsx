07b229267474339833f1183e19e0ab6cf02f1268
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-24 20:37:30 -0300

    refactor(components): Split Header component into small components

--- src/components/ChatContainer/Header/index.tsx
+++ src/components/ChatContainer/Header/index.tsx
@@ -1,17 +1,60 @@
-import type { FC } from 'react'
+import { FC, useState, FormEvent, ChangeEvent } from 'react'
+import { useAppSelector } from '@/hooks/redux'
 import { FaUser } from 'react-icons/fa'
+
+export const RoomInput: FC = () => {
+  const currentRoom = useAppSelector(state => state.room)
+  const [roomName, setRoomName] = useState<string>('')
+  const [editing, setEditing] = useState<boolean>(false)
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    setRoomName('')
+  }
+
+  const handleRoomNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setRoomName(event.target.value)
+  }
+
+  const handleFocus = () => {
+    setEditing(true)
+  }
+
+  const handleBlur = () => {
+    setEditing(false)
+  }
+
+  return (
+    <form className="w-full" onSubmit={handleSubmit}>
+      <input
+        className="w-full rounded-full bg-transparent py-2 text-base font-bold text-white
+      placeholder:bg-soft-dark placeholder:p-5 placeholder:text-soft-gray"
+        value={editing ? roomName : currentRoom.name}
+        onChange={handleRoomNameChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        placeholder="Código da sala"
+        title="Inserir código de sala privada"
+      />
+    </form>
+  )
+}
+
+export const UserCounter: FC = () => {
+  const currentRoom = useAppSelector(state => state.room)
+
+  return (
+    <p className="text-sm text-white">
+      {currentRoom.active_users?.length ?? 0}/5
+    </p>
+  )
+}
 
 const Header: FC = () => {
   return (
     <header className="flex h-16 items-center space-x-5 border-b-[1px] border-soft-dark px-10">
-      <input
-        className="w-full rounded-full bg-transparent py-2 text-base font-bold text-white
-        placeholder:bg-soft-dark placeholder:p-5 placeholder:text-soft-gray"
-        defaultValue="Sala 1"
-        placeholder="Código da sala"
-        title="Inserir código de sala privada"
-      />
-      <p className="text-sm text-white">1/5</p>
+      <RoomInput />
+      <UserCounter />
       <FaUser className="text-white" size={16} />
     </header>
   )
