c57b6d0b93ab65b35276d98b4a824d44fdbbb88f
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-25 06:55:52 -0300

    refactor(test): Change test to verify if formatTime respect a regex

--- src/__tests__/unit/format.test.ts
+++ src/__tests__/unit/format.test.ts
@@ -1,15 +1,18 @@
 import { formatTime } from '@/utils/format'
 
+const timeRegex: RegExp = /^(0\d|1\d|2[0-3]):[0-5]\d$/g
+
 describe('Testing methods of the "format" module', () => {
   test('"formatTime" must pad with leading zeros when necessary', () => {
     const date = new Date('2023-07-15T15:05:00')
     const time = formatTime(date)
     expect(time).toBe('15:05')
+    expect(time).toMatch(timeRegex)
   })
 
-  test('"formatTime" must return the formatted time correctly', () => {
+  test(`"formatTime" must respect ${timeRegex.source} regex`, () => {
     const dateString = '2023-07-15T08:30:00'
     const time = formatTime(dateString)
-    expect(time).toBe('08:30')
+    expect(time).toMatch(timeRegex)
   })
 })
