a4b0f7cfd77446495594c809d2ec80e2290748bd
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-25 01:54:04 -0300

    feat(components): Add listenMessages method to Chat component

--- src/components/ChatContainer/Chat/index.tsx
+++ src/components/ChatContainer/Chat/index.tsx
@@ -1,28 +1,39 @@
 import styles from './styles.module.css'
 import { FC, useState, useRef, useEffect } from 'react'
+import { useAppSelector } from '@/hooks/redux'
 import type { Message } from '@/entities/message'
+import { listenMessages } from '@/services/database/message'
 import VoidChat from '../VoidChat'
 import MessageCard from '../MessageCard'
 
 const Chat: FC = () => {
+  const currentRoom = useAppSelector(state => state.room)
   const [messages, setMessages] = useState<Message[]>([])
   const chatListRef = useRef<HTMLOListElement>(null)
+
+  useEffect(() => {
+    const handleMessagesListener = listenMessages(currentRoom.id, setMessages)
+    handleMessagesListener.on()
+    return () => {
+      handleMessagesListener.off()
+    }
+  }, [currentRoom.id])
 
   useEffect(() => {
     if (chatListRef.current) {
       chatListRef.current.scrollTop = chatListRef.current.scrollHeight
     }
-  }, [chatListRef])
+  }, [chatListRef, messages])
 
   const messagesSortedByMostRecent = messages.sort((a, b) => {
-    const aDate = new Date(a.sendTimestamp).getTime()
-    const bDate = new Date(b.sendTimestamp).getTime()
+    const aDate = new Date(a.send_timestamp).getTime()
+    const bDate = new Date(b.send_timestamp).getTime()
     return aDate - bDate
   })
 
   const renderMessages = () =>
     messagesSortedByMostRecent.map(message => (
-      <MessageCard key={message.sendTimestamp} message={message} />
+      <MessageCard key={message.send_timestamp} message={message} />
     ))
 
   if (messages.length === 0) return <VoidChat />
