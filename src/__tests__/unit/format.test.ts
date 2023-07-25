0bfbe87a950c4d750206746392f90d1ec07ccb96
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-25 06:57:32 -0300

    test(unit): Add a test to formatSendTimestamp method

--- src/__tests__/unit/format.test.ts
+++ src/__tests__/unit/format.test.ts
@@ -1,4 +1,4 @@
-import { formatTime } from '@/utils/format'
+import { formatTime, formatSendTimestamp } from '@/utils/format'
 
 const timeRegex: RegExp = /^(0\d|1\d|2[0-3]):[0-5]\d$/g
 
@@ -14,5 +14,12 @@ describe('Testing methods of the "format" module', () => {
     const dateString = '2023-07-15T08:30:00'
     const time = formatTime(dateString)
     expect(time).toMatch(timeRegex)
+  })
+
+  test('"formatSendTimestamp" must format date to yyyy-mm-dd HH-mm-ss', () => {
+    const dateString = '2023-05-01 09:04:07'
+    const date = new Date(dateString)
+    const formattedDate = formatSendTimestamp(date)
+    expect(formattedDate).toBe(dateString)
   })
 })
