800ea57f307ae507b97757c485f1deea687cd623
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-25 06:56:38 -0300

    feat(utils): Create formatSendTimestamp method

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -6,3 +6,14 @@ export const formatTime = (date: Date | string): string => {
 
   return `${hours}:${minutes}`
 }
+
+export const formatSendTimestamp = (date: Date): string => {
+  const year = date.getFullYear().toString()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
