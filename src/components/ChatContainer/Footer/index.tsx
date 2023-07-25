14f0d466965353618abdd291fbdbdb25e382db0e
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-07-25 07:12:23 -0300

    feat(components): Add setMessage method to Footer component

--- src/components/ChatContainer/Footer/index.tsx
+++ src/components/ChatContainer/Footer/index.tsx
@@ -1,20 +1,29 @@
 import { FC, useState, ChangeEvent, FormEvent } from 'react'
+import { useAppSelector } from '@/hooks/redux'
+import type { Message } from '@/entities/message'
+import { formatSendTimestamp } from '@/utils/format'
+import { setMessage } from '@/services/database/message'
 import { MdSend } from 'react-icons/md'
 
 const Footer: FC = () => {
-  const [message, setMessage] = useState<string>('')
+  const room = useAppSelector(state => state.room)
+  const user = useAppSelector(state => state.user)
+  const [messageBody, setMessageBody] = useState<string>('')
 
-  const handleMessageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setMessage(event.target.value)
+  const handleMessageBodyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMessageBody(event.target.value)
   }
 
   const submitMessage = async (event: FormEvent) => {
     event.preventDefault()
-    setMessage('')
-    const chatList = document.getElementById('chat-list')
-    if (chatList) {
-      chatList.scrollTop = chatList.scrollHeight
+    const message: Message = {
+      sender_id: user.id,
+      sender_nickname: user.nickname,
+      send_timestamp: formatSendTimestamp(new Date()),
+      body_text: messageBody
     }
+    await setMessage(room.id, message)
+    setMessageBody('')
   }
 
   return (
@@ -26,20 +35,20 @@ const Footer: FC = () => {
         <input
           className="w-full rounded-full bg-soft-dark px-[30px] py-[14px] text-white
         placeholder:text-soft-gray"
-          value={message}
-          onChange={handleMessageChange}
+          value={messageBody}
+          onChange={handleMessageBodyChange}
           placeholder="Mensagem"
           maxLength={300}
         />
-        {message.length > 0 && (
-          <p className="text-sm text-white">{message.length}/300</p>
+        {messageBody.length > 0 && (
+          <p className="text-sm text-white">{messageBody.length}/300</p>
         )}
         <button
           className="rounded-full bg-accent p-4 duration-150 disabled:bg-transparent"
           type="submit"
           title="Enviar mensagem"
           aria-label="Enviar mensagem"
-          disabled={message.length === 0}
+          disabled={messageBody.length === 0}
         >
           <MdSend className="text-white" size={20} />
         </button>
