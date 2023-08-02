4a7cf112e4dafdf5ae275e81936854257fd9b941
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-08-02 19:39:26 -0300

    test(e2e): Add a test to verify that the response body returns type User

--- cypress/e2e/api/user.cy.ts
+++ cypress/e2e/api/user.cy.ts
@@ -1,7 +1,16 @@
+const endpoint = '/api/user'
+
 describe('Testing "user" API endpoint', () => {
   it('Response status must be 200', () => {
-    cy.request('GET', '/api/user').then(response => {
+    cy.request('GET', endpoint).then(response => {
       expect(response.status).to.equal(200)
+    })
+  })
+
+  it('Response body must return "User" type as keys', () => {
+    cy.request('GET', endpoint).then(response => {
+      expect(response.body).to.have.key('user')
+      expect(response.body.user).to.have.keys('id', 'nickname')
     })
   })
 })
