32b4ca23ccca4fa7fbd2d0383199a6e1f4ab1704
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-08-06 19:02:50 -0300

    fix(e2e): Avoid tests from failing if there are no public rooms

--- cypress/e2e/actions/privateRoom.cy.ts
+++ cypress/e2e/actions/privateRoom.cy.ts
@@ -3,10 +3,11 @@ import { idRegex } from '@/utils/constants'
 describe('Testing private room actions', () => {
   it('Must create a new private room', () => {
     cy.visit('/')
-    cy.getElementByDataCy('rooms-list').then(() => {
-      cy.getElementByDataCy('create-private-room-button').click()
-      cy.getElementByDataCy('room-input').invoke('val').should('have.length', 4)
-      cy.getElementByDataCy('room-input').invoke('val').should('match', idRegex)
-    })
+    cy.intercept('/api/user').as('getUser')
+    cy.wait('@getUser')
+
+    cy.getElementByDataCy('create-private-room-button').click()
+    cy.getElementByDataCy('room-input').invoke('val').should('have.length', 4)
+    cy.getElementByDataCy('room-input').invoke('val').should('match', idRegex)
   })
 })
