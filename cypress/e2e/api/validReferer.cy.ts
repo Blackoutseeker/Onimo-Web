3edc81bd44b6b30059f4ae92220f257a855a3c53
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-08-22 13:31:05 -0300

    test(e2e): Add a test to check for methods other than POST

--- cypress/e2e/api/validReferer.cy.ts
+++ cypress/e2e/api/validReferer.cy.ts
@@ -1,12 +1,13 @@
 import { generateUser, generateId } from '@/utils/generate'
 import { formatSendTimestamp } from '@/utils/format'
 import { faker } from '@faker-js/faker'
+import { httpMethods } from '@/utils/constants'
 
 const endpoint = '/api/valid-referer'
 const fakeUser = generateUser()
 
 describe('Testing "valid-referer" API endpoint', () => {
-  it('Response status must be 200', () => {
+  it('Response status must be 200 if everything is correct', () => {
     const message = {
       senderId: fakeUser.id,
       senderNickname: fakeUser.nickname,
@@ -27,5 +28,20 @@ describe('Testing "valid-referer" API endpoint', () => {
       expect(response.status).to.equal(200)
       expect(response.body.message).to.equal('OK')
     })
+  })
+
+  it('Response status must be 405 for methods other than "POST"', () => {
+    const disallowedMethods = httpMethods.filter(method => method !== 'POST')
+
+    for (const method of disallowedMethods) {
+      cy.request({
+        method,
+        url: endpoint,
+        failOnStatusCode: false
+      }).then(response => {
+        expect(response.status).to.equal(405)
+        expect(response.body.message).to.equal('Method not allowed')
+      })
+    }
   })
 })
