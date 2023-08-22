dccff2ded8a4b81074eaf5b0119bae5f6afc033d
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-08-22 13:48:06 -0300

    test(e2e): Add a test to check for methods other than GET

--- cypress/e2e/api/delete.cy.ts
+++ cypress/e2e/api/delete.cy.ts
@@ -1,7 +1,8 @@
+import { httpMethods } from '@/utils/constants'
 const endpoint = '/api/delete'
 
 describe('Testing "delete" API endpoint', () => {
-  it('Response status must be 200', () => {
+  it('Response status must be 200 if everything is correct', () => {
     cy.request({
       method: 'GET',
       url: endpoint,
@@ -10,5 +11,20 @@ describe('Testing "delete" API endpoint', () => {
       expect(response.status).to.equal(200)
       expect(response.body.message).to.equal('TEST: data deleted successfully')
     })
+  })
+
+  it('Response status must be 405 for methods other than "GET"', () => {
+    const disallowedMethods = httpMethods.filter(method => method !== 'GET')
+
+    for (const method of disallowedMethods) {
+      cy.request({
+        method,
+        url: endpoint,
+        failOnStatusCode: false
+      }).then(response => {
+        expect(response.status).to.equal(405)
+        expect(response.body.message).to.equal('Method not allowed')
+      })
+    }
   })
 })
